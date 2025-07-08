9bdde1ebff0cb9000d134207479d2c2fab9dd407
JorelM5 <admin@dchaleco.com>
2025-07-07 21:10:42 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'coronados-cleaning-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // Add other assets as needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
